c18d6d1609ea669073d8108302cefaf43657e25b
zulus23 <zulus23@gmail.com>
2016-10-14 12:14:12 +0300

    add Norm time loading

---
+++ app/domain/NormaTimeLoading.scala
@@ -0,0 +1,14 @@
+package domain
+
+/**
+  * Created by Gukov on 14.10.2016.
+  *
+  * Нормативы времени погрузки
+  */
+
+
+
+class NormaTimeLoading(enterprise: Enterprise,packet:String,f1:String,f2:String)
+
+
+
